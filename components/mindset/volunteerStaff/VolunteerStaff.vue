ad48ce4cf13841fe83a3366d94da0a752886e82e
ilyas-g <ilyas15069i@gmail.com>
2025-06-16 16:30:09 +0200

    Ajout d'une image et ajout d'une couleur sur la langue active. + début de 'linté des descriptions des hotels

--- components/mindset/volunteerStaff/VolunteerStaff.vue
+++ components/mindset/volunteerStaff/VolunteerStaff.vue
@@ -4,9 +4,17 @@
             <img :src="imgSrc" :alt="title" />
         </div>
         <div class="cardStaff-body">
-            <h3 class="text-center">{{ title }}</h3>
-            <p class="text-center">{{ description }}</p>
-            <a :href="link" target="_blank" class="btn-ope video cardStaff-btn">Fill out the form</a>
+            <div class="title">
+                <h3 class="text-center">{{ title }}</h3>
+            </div>
+            
+            <div class="desc">
+                <!-- <p class="text-center">{{ description }}</p> -->
+                <p class="text-center adress">{{ adress }}</p>
+            </div>
+            <div class="button">
+                <a :href="link" target="_blank" class="btn-ope video cardStaff-btn">Fill out the form</a>
+            </div>
         </div>
     </div>
 </template>
@@ -16,6 +24,7 @@ defineProps<{
   format: string,
   imgSrc: string,
   description: string,
+  adress: string,
   link?: string
 }>()
 </script>
