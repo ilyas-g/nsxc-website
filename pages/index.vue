d3d364c6e0ce31318183424a3f583900c17af26d
ilyas-g <ilyas15069i@gmail.com>
2025-06-16 13:05:44 +0200

    Ajout d'une nouvelle image dans le paramètre SEO ogImage en guise de test (à remplacer par la suite)

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@ useSeoMeta({
   ogTitle: 'NSXC',
   description: 'With almost 10 years of experience in organizing competitive events , we make it a point of honour to be surrounded by passionate,volunteers and professionals. “By the community for the community “ is the credo that drive us. We created the “NSxC” collective to carry out this kind of project .',
   ogDescription: 'With almost 10 years of experience in organizing competitive events , we make it a point of honour to be surrounded by passionate,volunteers and professionals. “By the community for the community “ is the credo that drive us. We created the “NSxC” collective to carry out this kind of project .',
-  ogImage: '~/public/img/seo-screen.webp',
+  ogImage: 'https://images.start.gg/images/tournament/715965/image-1327951b994c5db7174073f192799454.png?ehk=%2FdIYPqXMiyxlZhAZ1pGUzh8GGrHibWs9QYP7u1%2Fm9wQ%3D&ehkOptimized=fMcijnmETeDACdR%2F8VwyEllzA71nhct3uAonhHhayuU%3D',
   twitterTitle: 'NSXC - MINDSET 25',
   twitterImage: '~/public/img/seo-nsxc.jfif',
   twitterCard: 'summary_large_image',
