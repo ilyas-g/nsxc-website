1932607668f32677a9226b38198f5902994f7bf3
ilyas-g <ilyas15069i@gmail.com>
2025-06-17 17:15:42 +0200

    Ajout des adresses des hotels et d'une vidéo qui loop dans la seconde section

--- components/mindset/hotelCard/HotelCard.vue
+++ components/mindset/hotelCard/HotelCard.vue
@@ -10,11 +10,10 @@
             
             <div class="desc">
                 <!-- <p class="text-center">{{ description }}</p> -->
-                 <p><span class="icon-instagram"></span> to venue</p>
+                 <!-- <p><span class="icon-instagram"></span> to venue</p> -->
                 <p class="text-center adress">{{ adress }}</p>
             </div>
             <div class="button">
-                ss
                 <a :href="link" target="_blank" class="btn-ope video cardStaff-btn">{{ t("bookARoom") }}</a>
             </div>
         </div>
