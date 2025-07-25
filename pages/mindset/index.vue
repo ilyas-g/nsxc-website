e5a3f7caae776328c6d93eebb9c83589981ae530
ilyas-g <ilyas15069i@gmail.com>
2025-07-25 19:40:31 +0200

    SEO again

--- pages/mindset/index.vue
+++ pages/mindset/index.vue
@@ -44,16 +44,14 @@ const toggleContact = (toggleValue: boolean) => {
 };
 
 useSeoMeta({
-  title: 'NSXC - MINDSET 266666666666666666666666666666665',
+  title: 'NSXC - MINDSET 25',
+  ogTitle: 'NSXC - MINDSET 25',
   keywords: 'Mindset, NSXC, tournois, événement, jeux vidéo, motivation, détermination, soi, meilleur, mentalité, attitude, comportement, émotions, feeling, fihgitng games, jeux de combat, Street Fighter, Street Fighter 6, 2XKO, Riot, Tekken 8, Tekken, COTW',
-  ogTitle: 'NSXC - MINDSET 25666666666',
   description: 'Mindset was born in 2024 with the desire to make its contribution in an ecosystem where esport is taking more and more place; bring a vision that invites introspection and consider its place within this community.',
   ogDescription: 'Mindset was born in 2024 with the desire to make its contribution in an ecosystem where esport is taking more and more place; bring a vision that invites introspection and consider its place within this community.',
   ogImage: '~/public/img/seo-screen.webp',
   twitterTitle: 'NSXC - MINDSET 25',
-  twitterImage: 'https://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg',
   twitterCard: 'summary_large_image',
-  twitterImageAlt: 'NSXC - MINDSET 25',
   twitterSite: '@_NSxC',
   twitterCreator: '@iamarktall',
   twitterDescription: 'Mindset was born in 2024 with the desire to make its contribution in an ecosystem where esport is taking more and more place; bring a vision that invites introspection and consider its place within this community.'
