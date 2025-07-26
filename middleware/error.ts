dbd8fa0e3250d2ab71cc7db96f9eb522e9c08da9
ilyas-g <ilyas15069i@gmail.com>
2025-07-26 12:06:32 +0200

    Modification de la condition du middleware (ne prend plus en compte la page des règles)

--- middleware/error.ts
+++ middleware/error.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.path !== '/mindset' && to.path !== '/mindset/rules') {
+  if (to.path !== '/mindset') {
     // setting the redirect code to '301 Moved Permanently'
     return navigateTo('/mindset', { redirectCode: 301 })
   }
