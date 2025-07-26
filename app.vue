adadbe0861cff6e256cbe192fd5c7191b4953629
ilyas-g <ilyas15069i@gmail.com>
2025-07-26 12:24:13 +0200

    Ajout d'un plugin

--- app.vue
+++ app.vue
@@ -5,4 +5,19 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+import { showError } from '#imports'
 
+const route = useRoute()
+if ('setup' in route.query) {
+  throw new Error('error in setup')
+}
+if ('mounted' in route.query) {
+  onMounted(() => {
+    throw new Error('error in mounted')
+  })
+}
+function triggerError() {
+  throw new Error('manually triggered error')
+}
+</script>
