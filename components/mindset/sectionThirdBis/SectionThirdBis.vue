4d86a3f0b6914bfc8fb774a290f7cf9910cfa86e
ilyas-g <ilyas15069i@gmail.com>
2026-05-20 07:41:39 +0200

    Mindset 2026 -changements apportés dans la page

--- components/mindset/sectionThirdBis/SectionThirdBis.vue
+++ components/mindset/sectionThirdBis/SectionThirdBis.vue
@@ -11,13 +11,10 @@
                         <div class="text-center more-info">
                             <Game  :imgSrc=sf6 support="PC" format="Solo" description="Street Fighter 6 logo" :linkBtn="localePath('/mindset/rules#sf6rules')" />
                             <Game  :imgSrc=t8 support="Playstation 5" format="Solo" description="Tekken 8 logo" :linkBtn="localePath('/mindset/rules#t8rules')" />
-                            <Game  :imgSrc=ffcotw support="Playstation 5" format="Solo" description="Fatal Fury: City of The Wolves logo" :linkBtn="localePath('/mindset/rules#ffcotwrules')"/>
-                            <Game  :imgSrc=ggst :protour=arcc support="Playstation 4 Pro" format="Solo" description="Guilty Gear Strive logo" :linkBtn="localePath('/mindset/rules#ggstrules')"/>
-                            <Game  :imgSrc=gbvsr :protour=arcc support="Playstation 4 Pro" format="Solo" description="Granblue Fantasy Versus: Rising logo" :linkBtn="localePath('/mindset/rules#gbvsrrules')" />
-                            <Game  :imgSrc=ggxrdrev2 support="Playstation 4" format="Solo" description="Guilty Gear Xrd REV 2 logo" :linkBtn="localePath('/mindset/rules#ggxrdrev2rules')" />
-                            <Game  :imgSrc=hxh support="Playstation 5" format="Solo" description="Hunter X Hunter NEN IMPACT logo" :linkBtn="localePath('/mindset/rules#hxhrules')" />
-                            <Game  :imgSrc=xko :protour=xko_firstImpact classWT="firstImpact" support="PC" :format=xkoMixte description="2XKO logo" :linkBtn="localePath('/mindset/rules#2xko1v1rules')" />
-                            <Game group=true :imgSrc=xko classWT="firstImpact" support="PC" format="Duo" description="2XKO logo" :linkBtn="localePath('/mindset/rules#2xko2v2rules')" />
+                            <Game  :imgSrc=ggst :protour="arcc" support="Playstation 4 Pro" format="Solo" description="Guilty Gear Strive logo" :linkBtn="localePath('/mindset/rules#ggstrules')"/>
+                            <Game  :imgSrc=gbvsr support="Playstation 4 Pro" format="Solo" description="Granblue Fantasy Versus: Rising logo" :linkBtn="localePath('/mindset/rules#gbvsrrules')" />
+                            <Game  :imgSrc=xko classWT="firstImpact" support="PC" :format=xkoMixte description="2XKO logo" :linkBtn="localePath('/mindset/rules#2xko1v1rules')" />
+                            <Game  :imgSrc=marveltokon support="TBA" format="Solo" description="Marvel Tokon logo" :linkBtn="localePath('/mindset/rules#marveltokon')"/>
                         </div>
                     </div>
                 </div>
@@ -32,8 +29,8 @@ import xko_double from '~/public/img/games_logos/2xko-2v2.webp'
 import t8 from '~/public/img/games_logos/t8.webp'
 import ggst from '~/public/img/games_logos/ggst.webp'
 import gbvsr from '~/public/img/games_logos/gbvsr.webp'
-import ffcotw from '~/public/img/games_logos/ffcotw.webp'
-import arcc from '~/public/img/arcworldtour2025-2026.webp'
+import marveltokon from '~/public/img/games_logos/marveltokon.webp'
+import arcc from '~/public/img/arcworldtour2026-2027.webp'
 import xko_firstImpact from '~/public/img/2XKO_FirstImpactLogo_DarkBG.png'
 import ggxrdrev2 from '~/public/img/games_logos/GGXRD-R2.webp'
 import hxh from '~/public/img/games_logos/hxh.webp'
